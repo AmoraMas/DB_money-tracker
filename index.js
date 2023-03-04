620cd275a1301a0958e88a5d3b8b9230597bac64
AmoraMas <amoramas@protonmail.com>
2023-03-03 17:18:07 -0700

    updated frontend

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
-import express from express;
-const app = express();
-app.use(cors());
 
+// Declare Variables
+const route = 'https://money-test.onrender.com';
+let $tableValue;
+let $typeValue;
+let $accountID;
+let $depositID;
+
+// Function to get new values from form
+function getFormInfo () {
+    $tableValue = $('#table').val();
+    $typeValue = $('#type').val();
+    $accountID = $('#accountID').val();
+    $depositID = $('#depositID').val();
+}
+
+// Assign actions to buttons
 let $submitButton = $('.submitButton');
 $submitButton.click(function () {
+  getFormInfo();
   respondToForm();
 });
 let $clearButton = $('.clearButton');
@@ -11,22 +25,23 @@ $clearButton.click(function () {
   $('#response-area').empty();
 });
 
+// DO SOMETHING when 'Submit Request' is clicked
 function respondToForm () {
-    if ($('#type').val() == 'GET') {
-        console.log('GETting all data from table: ', $('#table').val());
+    if ($typeValue == 'GET') {
+        console.log('GETting all the data from table: ', $tableValue);
         let searchString;
-        if ($('#table').val() == 'deposits' && $('#accountID').val() != '') {
-            console.log('accountID: ', $('#accountID').val());
-            searchString = '/accounts/' + $('#accountID').val() + '/' + $('#table').val()
+        if ($tableValue == 'deposits' && $accountID != '') {
+            console.log('accountID: ', $accountID);
+            searchString = route + '/api/accounts/' + $accountID + '/' + $tableValue;
         }
-        else if ($('#table').val() == 'accounts' && $('#accountID').val() != null) {
-            searchString = '/api/' + $('#table').val()+'/' + $('#accountID').val();
+        else if ($tableValue == 'accounts' && $accountID != '') {
+            searchString = route + '/api/' + $tableValue+'/' + $accountID;
         }
-        else if ($('#table').val() == 'deposits' && $('#depositID').val() != null) {
-            searchString = '/api/' + $('#table').val() + '/' + $('#depositID').val();
+        else if ($tableValue == 'deposits' && $depositID != '') {
+            searchString = route + '/api/' + $tableValue + '/' + $depositID;
         }
         else {
-            searchString = '/api/' + $('#table').val()
+            searchString = route + '/api/' + $tableValue;
         }
         console.log('searchString: ', searchString);
         $.get(searchString, (data) => { 
